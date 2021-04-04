13f8fec18ab0c7a0bbbadd484fe903f3ba7ad4c3
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-04-04 10:59:07 -0300

    Criação dos Repositories de Pessoa e Vacina

---
+++ src/main/java/com/pedro/vacinaagora/repositories/VacinaRepository.java
@@ -0,0 +1,9 @@
+package com.pedro.vacinaagora.repositories;
+
+import com.pedro.vacinaagora.entities.Vacina;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VacinaRepository extends JpaRepository<Vacina, Integer> {
+    
+}
